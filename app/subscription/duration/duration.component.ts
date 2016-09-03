4c7fa28ede86a5fa2fc14bf16bab695ba653a049
dhilt <d.alexanov@gmail.com>
2016-09-03 17:04:57 +0300

    Add and attach DurationComponent with a separate template

---
+++ app/subscription/duration/duration.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'duration',
+  templateUrl: 'app/subscription/duration/duration.component.html'
+})
+export class DurationComponent { }
--- app/subscription/subscription.module.ts
+++ app/subscription/subscription.module.ts
@@ -4,10 +4,11 @@ import {CommonModule} from '@angular/common';
 import {SubscriptionComponent} from './subscription.component';
 import {MentorsComponent} from './mentors_and_courses/mentors.component';
 import {CoursesComponent} from './mentors_and_courses/courses.component';
+import {DurationComponent} from './duration/duration.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [SubscriptionComponent, MentorsComponent, CoursesComponent],
+  declarations: [SubscriptionComponent, MentorsComponent, CoursesComponent, DurationComponent],
   exports: [SubscriptionComponent]
 })
 
