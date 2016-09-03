030d6b646f24802a60a719ce53d8f1feebd09652
dhilt <d.alexanov@gmail.com>
2016-09-03 17:02:20 +0300

    add SubscriptionModule to the AppModule

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SubscriptionModule } from './subscription/subscription.module';
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports: [ BrowserModule ],
+  imports: [ BrowserModule, SubscriptionModule ],
   declarations: [ AppComponent ],
   bootstrap: [ AppComponent ]
 })
