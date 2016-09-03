4969f66ffbfbb843e4ad2730ef62970034fe8ebd
dhilt <d.alexanov@gmail.com>
2016-09-03 17:04:57 +0300

    Attach Mentors and Courses components to SubscriptionModule

--- app/subscription/subscription.module.ts
+++ app/subscription/subscription.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
 import {SubscriptionComponent} from './subscription.component';
+import {MentorsComponent} from './mentors_and_courses/mentors.component';
+import {CoursesComponent} from './mentors_and_courses/courses.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [SubscriptionComponent],
+  declarations: [SubscriptionComponent, MentorsComponent, CoursesComponent],
   exports: [SubscriptionComponent]
 })
 
