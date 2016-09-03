80e4367df227d88d8db1acb01ffe14f3b3ae3b90
dhilt <d.alexanov@gmail.com>
2016-09-03 17:04:57 +0300

    Add PaymentModule to the AppModule

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { SubscriptionModule } from './subscription/subscription.module';
+import { PaymentModule } from './payment/payment.module';
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports: [ BrowserModule, SubscriptionModule ],
+  imports: [ BrowserModule, SubscriptionModule, PaymentModule ],
   declarations: [ AppComponent ],
   bootstrap: [ AppComponent ]
 })
