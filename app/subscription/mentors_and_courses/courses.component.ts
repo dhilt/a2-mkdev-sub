715baa30019fab657c31075641f9eaee27d401e0
dhilt <d.alexanov@gmail.com>
2016-09-03 17:04:57 +0300

    Add Mentors and Courses components and templates

---
+++ app/subscription/mentors_and_courses/courses.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'courses',
+  templateUrl: 'app/subscription/mentors_and_courses/courses.component.html'
+})
+export class CoursesComponent { }
---
+++ app/subscription/mentors_and_courses/mentors.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mentors',
+  templateUrl: 'app/subscription/mentors_and_courses/mentors.component.html'
+})
+export class MentorsComponent { }
