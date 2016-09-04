3006867f03740cd0e24b6fc381c779b539932248
dhilt <d.alexanov@gmail.com>
2016-09-04 13:59:30 +0300

    SubscriptionComponent mentors and courses initializing

--- app/subscription/subscription.component.ts
+++ app/subscription/subscription.component.ts
@@ -1,13 +1,24 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Mentor} from './mentors_and_courses/mentor';
+import {MentorService} from './mentors_and_courses/mentor.service';
 import {Course} from './mentors_and_courses/course';
+import {CourseService} from './mentors_and_courses/course.service';
 
 @Component({
   selector: 'subscription',
   templateUrl: 'app/subscription/subscription.component.html'
 })
 
-export class SubscriptionComponent {
+export class SubscriptionComponent implements OnInit {
+
   public mentors: Mentor[];
   public courses: Course[];
+
+  constructor(private mentorService: MentorService, private courseService: CourseService) {
+  }
+
+  ngOnInit(): void {
+    this.mentorService.getMentors().then(result => this.mentors = result);
+    this.courseService.getCourses().then(result => this.courses = result);
+  }
 }
