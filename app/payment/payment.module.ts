94b1623ab6cc9b6e558ca5c1c244adfe1b6a5944
NA <d.alexanov@gmail.com>
2016-09-05 04:55:10 +0300

    add angular2-text-mask directive via npm

--- app/payment/payment.module.ts
+++ app/payment/payment.module.ts
@@ -1,13 +1,14 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
+import MaskedInput from 'angular2-text-mask';
 
 import {PaymentComponent} from './payment.component';
 import {HistoryComponent} from './history/history.component';
 
 @NgModule({
   imports: [CommonModule, FormsModule],
-  declarations: [PaymentComponent, HistoryComponent],
+  declarations: [MaskedInput, PaymentComponent, HistoryComponent],
   exports: [PaymentComponent]
 })
 
